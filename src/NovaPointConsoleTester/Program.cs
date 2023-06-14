27f1c5708053b551f3118f995f124a9a83af52e4
Barbarur <alvaro.avila@protonmail.com>
2023-06-15 00:37:26 +0800

    Adding Testing project

---
+++ src/NovaPointConsoleTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using NovaPointLibrary;
+using NovaPointLibrary.Commands.Authentication;
+using NovaPointLibrary.Solutions;
+using System;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
+
+namespace NovaPointConsoleTester
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            await new SolutionsTester(UILog).TestSolution();
+
+            Console.WriteLine("Byebye, World!");
+
+        }
+
+        static void UILog(LogInfo logInfo)
+        {
+            if (!string.IsNullOrEmpty(logInfo.MainClassInfo)) { Console.WriteLine($"{logInfo.MainClassInfo}\n"); }
+            if (logInfo.PercentageProgress != -1)
+            {
+                Console.WriteLine($"Progress: {logInfo.PercentageProgress}\n");
+                Console.WriteLine($"{logInfo.PendingTime}\n");
+            }
+
+        }
+    }
+}
