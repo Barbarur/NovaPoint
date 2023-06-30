757dd75e72658fe3a8c480a7faf0726544630aa6
Barbarur <alvaro.avila@protonmail.com>
2023-06-30 23:20:15 +0800

    Apply Cancellation Token to GetSPOUser

---
+++ src/NovaPointLibrary/Commands/SharePoint/User/GetSPOUser.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.SharePoint.Client;
+using NovaPointLibrary.Commands.Authentication;
+using NovaPointLibrary.Solutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static NovaPointLibrary.Commands.User.GetUser;
+
+namespace NovaPointLibrary.Commands.SharePoint.User
+{
+    internal class GetSPOUser
+    {
+        private LogHelper _LogHelper;
+        private readonly AppInfo _appInfo;
+        private readonly string AccessToken;
+        internal GetSPOUser(LogHelper logHelper, AppInfo appInfo, string accessToken)
+        {
+            _LogHelper = logHelper;
+            _appInfo = appInfo;
+            AccessToken = accessToken;
+        }
+        // Reference:
+        // https://github.com/pnp/powershell/blob/dev/src/Commands/Base/PipeBinds/UserPipeBind.cs
+        internal Microsoft.SharePoint.Client.User? CSOMSingle(string siteUrl, string userUPN)
+        {
+            _appInfo.IsCancelled();
+            string methodName = $"{GetType().Name}.CSOMSingle";
+            _LogHelper.AddLogToTxt(methodName, $"Start obtaining User '{userUPN}' from Site '{siteUrl}'");
+
+            using var clientContext = new ClientContext(siteUrl);
+            clientContext.ExecutingWebRequest += (sender, e) =>
+            {
+                e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + AccessToken;
+            };
+
+            var retrievalExpressions = new Expression<Func<Microsoft.SharePoint.Client.User, object>>[]
+            {
+                u => u.Id,
+                u => u.Title,
+                u => u.LoginName,
+                u => u.UserPrincipalName,
+                u => u.Email,
+                u => u.IsShareByEmailGuestUser,
+                u => u.IsSiteAdmin,
+                u => u.UserId,
+                u => u.IsHiddenInUI,
+                u => u.PrincipalType,
+                u => u.Alerts.Include(
+                    a => a.Title,
+                    a => a.Status),
+                u => u.Groups.Include(
+                    g => g.Id,
+                    g => g.Title,
+                    g => g.LoginName)
+            };
+
+            string userLoginName = "i:0#.f|membership|" + userUPN;
+            _LogHelper.AddLogToTxt(methodName, $"User LoginName '{userLoginName}'");
+
+            try
+            {
+
+                Microsoft.SharePoint.Client.User user = clientContext.Web.SiteUsers.GetByLoginName(userLoginName);
+
+                clientContext.Load(user, retrievalExpressions);
+                clientContext.ExecuteQueryRetry();
+
+                _LogHelper.AddLogToTxt(methodName, $"User '{userUPN}' found in Site '{siteUrl}'");
+                return user;
+
+            }
+            catch
+            {
+                _LogHelper.AddLogToTxt(methodName, $"User '{userUPN}' no found in Site '{siteUrl}'");
+                return null;
+            }
+
+        }
+
+
+        // Reference:
+        // https://pnp.github.io/powershell/cmdlets/Get-PnPUser.html
+        // https://github.com/pnp/powershell/blob/dev/src/Commands/Principals/GetUser.cs
+        // https://www.sharepointdiary.com/2017/02/sharepoint-online-get-all-users-using-powershell.html
+        internal List<Microsoft.SharePoint.Client.User> CSOMAll(string siteUrl, bool WithRightsAssigned = false, bool WithRightsAssignedDetailed = false)
+        {
+            _appInfo.IsCancelled();
+            string methodName = $"{GetType().Name}.CSOMAll";
+            _LogHelper.AddLogToTxt(methodName, $"Start getting Users for Site '{siteUrl}'");
+
+            using var clientContext = new ClientContext(siteUrl);
+            clientContext.ExecutingWebRequest += (sender, e) =>
+            {
+                e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + AccessToken;
+            };
+
+            var retrievalExpressions = new Expression<Func<Microsoft.SharePoint.Client.User, object>>[]
+            {
+                u => u.Id,
+                u => u.Title,
+                u => u.LoginName,
+                u => u.UserPrincipalName,
+                u => u.Email,
+                u => u.IsShareByEmailGuestUser,
+                u => u.IsSiteAdmin,
+                u => u.UserId,
+                u => u.IsHiddenInUI,
+                u => u.PrincipalType,
+                u => u.Alerts.Include(
+                    a => a.Title,
+                    a => a.Status),
+                u => u.Groups.Include(
+                    g => g.Id,
+                    g => g.Title,
+                    g => g.LoginName)
+            };
+
+            UserCollection collUsers = clientContext.Web.SiteUsers;
+            clientContext.Load(collUsers, u => u.Include(retrievalExpressions));
+
+            clientContext.ExecuteQuery();
+
+            List<Microsoft.SharePoint.Client.User> listUsersReturned = new();
+            listUsersReturned.AddRange(clientContext.Web.SiteUsers);
+            listUsersReturned.RemoveAll(u => u.Title == "System Account" || u.Title == "SharePoint App" || u.Title == "NT Service\\spsearch");
+            
+            _LogHelper.AddLogToTxt(methodName, $"Finish getting Users for Site '{siteUrl}'");
+            return listUsersReturned;
+        }
+    }
+}
