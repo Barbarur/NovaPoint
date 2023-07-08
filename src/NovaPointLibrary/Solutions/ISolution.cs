ad510ca954b10206db17ca30d0b431988ceeb1e0
Barbarur <alvaro.avila@protonmail.com>
2023-07-08 20:25:16 +0800

    Adding Solution Interface

---
+++ src/NovaPointLibrary/Solutions/ISolution.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaPointLibrary.Solutions
+{
+    public interface ISolution
+    {
+        public static string s_solutionName = "Solution";
+        public static string s_solutionDocs = "https://github.com/Barbarur/NovaPoint/wiki/";
+
+        public async Task RunAsync()
+        {
+        }
+
+    }
+}
