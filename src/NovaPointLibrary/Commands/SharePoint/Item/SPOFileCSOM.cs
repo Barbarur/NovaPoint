09ac1e27c7a8be660c0e6321a19972145e9c29ef
Barbarur <alvaro.avila@protonmail.com>
2024-07-17 21:28:52 +0800

    Implement file copy method

--- src/NovaPointLibrary/Commands/SharePoint/Item/SPOFileCSOM.cs
+++ src/NovaPointLibrary/Commands/SharePoint/Item/SPOFileCSOM.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.SharePoint.Client;
+using Newtonsoft.Json;
+using NovaPointLibrary.Commands.Utilities;
+using NovaPointLibrary.Commands.Utilities.RESTModel;
 using NovaPointLibrary.Solutions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using File = Microsoft.SharePoint.Client.File;
 
 namespace NovaPointLibrary.Commands.SharePoint.Item
@@ -149,5 +147,99 @@ namespace NovaPointLibrary.Commands.SharePoint.Item
             return newName + extension;
         }
 
+
+        // Reference:
+        // https://gist.github.com/zplume/21248c3a8a5f840a366722442cf9ee97
+        // https://learn.microsoft.com/en-us/sharepoint/dev/apis/spod-copy-move-api
+        
+        // Keep in mind potential same folder optimization for future updates
+        internal async Task CopyAsync(string siteUrl, string sourceServerRelativeUrl, string destinationServerRelativeUrl, bool noWait)
+        {
+            _appInfo.IsCancelled();
+            _logger.LogTxt(GetType().Name, $"Coping file '{sourceServerRelativeUrl}' from site '{siteUrl}' to '{destinationServerRelativeUrl}.");
+
+            Uri sourceUri = new(new(siteUrl), EncodePath(sourceServerRelativeUrl));
+            Uri targetUri = new(new(siteUrl), EncodePath(destinationServerRelativeUrl));
+
+            string api = siteUrl + "/_api/site/CreateCopyJobs";
+
+            var x = new
+            {
+                exportObjectUris = new[] { sourceUri },
+                destinationUri = targetUri,
+                options = new
+                {
+                    IgnoreVersionHistory = true,
+                    IsMoveMode = false,
+                    AllowSchemaMismatch = true,
+                    NameConflictBehavior = 0,
+                    AllowSmallerVersionLimitOnDestination = true
+                }
+            };
+
+            var content = JsonConvert.SerializeObject(x);
+
+            string response = await new RESTAPIHandler(_logger, _appInfo).PostAsync(api, content);
+
+            var resultCollection = JsonConvert.DeserializeObject<RESTResultCollection<RESTCreateCopyJobs>>(response);
+
+            if (resultCollection == null || !resultCollection.Items.Any())
+            {
+                throw new("Copy job creation response is empty");
+            }
+
+            var createCopyJob = resultCollection.Items.First();
+
+            var copyJobInfo = new
+            {
+                copyJobInfo = createCopyJob
+            };
+
+            var contentcopyJobInfo = JsonConvert.SerializeObject(copyJobInfo);
+
+            api = siteUrl + "/_api/site/GetCopyJobProgress";
+            response = await new RESTAPIHandler(_logger, _appInfo).PostAsync(api, contentcopyJobInfo);
+
+            if (noWait)
+            {
+                return;
+            }
+
+            var copyJobProgress = JsonConvert.DeserializeObject<RESTCopyJobProgress>(response);
+            if (copyJobProgress == null)
+            {
+                throw new("Copy job progress respose is empty");
+            }
+            
+            while (copyJobProgress.JobState != 0)
+            {
+                // sleep 1 second
+                await Task.Delay(1000);
+                response = await new RESTAPIHandler(_logger, _appInfo).PostAsync(api, contentcopyJobInfo);
+
+                copyJobProgress = JsonConvert.DeserializeObject<RESTCopyJobProgress>(response);
+                if (copyJobProgress == null)
+                {
+                    throw new("Copy job progress respose is empty");
+                }
+            }
+
+        }
+
+        private string EncodePath(string path)
+        {
+            var parts = path.Split("/");
+            var encodedPath = string.Join("/", parts.Select(p => Uri.EscapeDataString(p)));
+            _logger.LogTxt(GetType().Name, $"Encoded path {encodedPath}");
+            return encodedPath;
+        }
+    }
+
+    internal class CopyJobs
+    {
+        public string EncryptionKey { get; set; }
+        public string JobId { get; set; }
+        public string JobQueueUri { get; set; }
+        public string[] SourceListItemUniqueIds { get; set; }
     }
 }
