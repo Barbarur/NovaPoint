5ee8822223201281bdefd87063a9bc7843ce094a
Barbarur <alvaro.avila@protonmail.com>
2026-02-10 23:39:15 +0100

    feat: adding new pages for running solutions when using multi-tenant

---
+++ src/NovaPointWPF/Pages/Solutions/SolutionPreparationPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using NovaPointLibrary.Solutions;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace NovaPointWPF.Pages.Solutions
+{
+    public partial class SolutionPreparationPage : Page
+    {
+        static ReaderWriterLock rwl = new ReaderWriterLock();
+        private readonly ISolutionFormTest _solutionForm;
+
+        public SolutionPreparationPage(ISolutionFormTest solutionForm)
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            SolutionHeader.SolutionTitle = solutionForm.SolutionName;
+            SolutionHeader.SolutionCode = solutionForm.SolutionCode;
+            SolutionHeader.SolutionDocs = solutionForm.SolutionDocs;
+
+            SolutionFormFrame.Content = solutionForm;
+
+            _solutionForm = solutionForm;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow? mainWindow = Application.Current.MainWindow as MainWindow;
+
+            if (mainWindow is not null) { Application.Current.MainWindow.Content = mainWindow.MainPage; }
+        }
+
+        private async void RunButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            BackButton.IsEnabled = false;
+            RunButton.IsEnabled = false;
+
+
+            try
+            {
+                SolutionHandler handler = new(_solutionForm.SolutionCreate, _solutionForm.GetParameters(), AppSelector.GetClient());
+
+                SolutionProgressView sViewer = new(handler);
+                SolutionProgressViewFrame.Content = sViewer;
+
+                await sViewer.RunSolutionAsync();
+            }
+            catch (Exception ex)
+            {
+                //UILog(LogInfo.ErrorNotification($"Exception: {ex.Message}"));
+                //UILog(LogInfo.ErrorNotification($"StackTrace: {ex.StackTrace}"));
+            }
+
+            BackButton.IsEnabled = true;
+            RunButton.IsEnabled = true;
+        }
+
+    }
+}
---
+++ src/NovaPointWPF/Pages/Solutions/SolutionProgressView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using NovaPointLibrary.Solutions;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace NovaPointWPF.Pages.Solutions
+{
+    public partial class SolutionProgressView : UserControl
+    {
+        public SolutionHandler Handler;
+
+        public SolutionProgressView(SolutionHandler handler)
+        {
+            DataContext = handler;
+            Handler = handler;
+
+            InitializeComponent();
+        }
+
+        internal async Task RunSolutionAsync()
+        {
+            CancelButton.IsEnabled = true;
+            FolderButton.IsEnabled = false;
+
+            try
+            {
+                await Handler.RunSolution();
+            }
+            catch (Exception ex)
+            {
+                Handler.UILog(LogInfo.ErrorNotification(ex.Message));
+            }
+
+            Handler.PendingTime = "Completed!";
+            FolderButton.IsEnabled = true;
+            CancelButton.IsEnabled = false;
+        }
+
+        private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Handler.UILog(LogInfo.ErrorNotification("Canceling solution. Please wait while we stop all the processes."));
+            Handler.CancelTokenSource.Cancel();
+            Handler.CancelTokenSource.Dispose();
+            CancelButton.IsEnabled = false;
+        }
+
+        private void FolderClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (System.IO.Directory.Exists(Handler.SolutionFolder))
+            {
+                try { Process.Start("explorer.exe", Handler.SolutionFolder); }
+                catch (Exception ex)
+                {
+                    Handler.UILog(LogInfo.ErrorNotification(ex.Message));
+                }
+            }
+            ;
+        }
+    }
+}
