c292f24b023a5b6b993e9bbe1053fcf32286c8f3
Barbarur <novapointapp@protonmail.com>
2026-06-14 09:32:24 +0200

    add: PermissionsAssessment Class and GraphConstants Class

---
+++ src/NovaPointLibrary/Commands/Directory/Applications/PermissionAssessment.cs
@@ -0,0 +1,6 @@
+namespace NovaPointLibrary.Commands.Directory.Applications;
+
+internal static class PermissionAssessment
+{
+    internal static readonly string[] ExcessiveKeywords = ["write", "edit", "manage", "fullcontrol"];
+}
---
+++ src/NovaPointLibrary/Commands/Utilities/GraphConstants.cs
@@ -0,0 +1,6 @@
+namespace NovaPointLibrary.Commands.Utilities;
+
+internal static class GraphConstants
+{
+    internal static readonly Guid MicrosoftTenantId = new("f8cdef31-a31e-4b4a-93e4-5f571e91255a");
+}
